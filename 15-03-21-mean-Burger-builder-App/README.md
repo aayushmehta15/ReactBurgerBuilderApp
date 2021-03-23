1a6fd8ab04e26a60c33accb5a7bdac87ed1b8121
Aayush <aayush.mehta@tothenew.com>
2021-03-23 06:12:42 +0530

    Added Redux

--- 15-03-21-mean-Burger-builder-App/README.md
+++ 15-03-21-mean-Burger-builder-App/README.md
@@ -1,6 +1,6 @@
-#BURGER BUILDER APP
+# BURGER BUILDER APP
 
-##Section Name: Getting Started with basic project
-##Date: 11-03-21
+## Section Name: Getting Started With Redux
+## Date: 22-03-21
 
 
--- 15-03-21-mean-Burger-builder-App/src/hoc/Layout/Layout.js
+++ 15-03-21-mean-Burger-builder-App/src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
-import Aux from "../AUX/Aux";
-import classes from "./Layout.module.css";
+import Aux from "../Aux/Aux";
+import classes from "../Layout/Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
